c4b9958207fe0a370f5f30b57925b9b80777f42b
Jeff Stevens <jefste@gmail.com>
2015-02-03 12:25:11 -0500

    Add comment and clean up to plot2.R for better readability

--- plot2.R
+++ plot2.R
@@ -1,6 +1,10 @@
+#read in data and store in data frame hpc
 hpc<-read.csv(file='household_power_consumption.txt',sep=';',
+              #set '?' values to 'NA'
               na.strings = '?',
-              colClasses=c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),
+              #gives data the appropriate class
+              colClasses=c('character','character','numeric','numeric',
+                           'numeric','numeric','numeric','numeric','numeric'),
 )
 
 #create subset for the 2 days of interest
@@ -8,10 +12,12 @@ hpc.2day<-subset(hpc,hpc['Date']=='1/2/2007'|hpc['Date']=='2/2/2007')
 
 #paste the time and date columns of the subsetted data together
 time.date.2day<-paste(hpc.2day$Date,hpc.2day$Time)
+
 #use strptime to put variable in POSIXlt class
 time.date.2day<-strptime(time.date.2day,format='%d/%m/%Y %H:%M:%S')
 
 #plot 2
-
+#create a scatter plot of global active power vs time, with a solid line
 plot(x=time.date.2day,y=hpc.2day$Global_active_power,type='l',
+     #labels plot per instructions
      ylab='Global Active Power (kilowatts)',xlab='')
