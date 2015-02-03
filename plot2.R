52634bc52811eba55b9c17492c37bb8b10c36064
Jeff Stevens <jefste@gmail.com>
2015-02-03 12:45:51 -0500

    Add code that saves the plot as a PNG and also add the output plot2.png to the repo

--- plot2.R
+++ plot2.R
@@ -16,8 +16,15 @@ time.date.2day<-paste(hpc.2day$Date,hpc.2day$Time)
 #use strptime to put variable in POSIXlt class
 time.date.2day<-strptime(time.date.2day,format='%d/%m/%Y %H:%M:%S')
 
+
 #plot 2
+#device to create PNG file
+png("plot2.png", width=480, height=480, units='px')
+
 #create a scatter plot of global active power vs time, with a solid line
 plot(x=time.date.2day,y=hpc.2day$Global_active_power,type='l',
      #labels plot per instructions
      ylab='Global Active Power (kilowatts)',xlab='')
+
+#close the plot2.png device
+dev.off() 
